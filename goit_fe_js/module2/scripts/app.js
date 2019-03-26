de67feba11c3cd3563f04343f60dbe315d550a8c
Max <max.yar.eli@gmail.com>
2019-03-26 16:45:38 +0200

    v3

--- goit_fe_js/module2/scripts/app.js
+++ goit_fe_js/module2/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // task1
 
@@ -6,55 +6,46 @@ let input = 0;
 const numbers = [];
 let total = 0;
 
-while(true)
-{
-    input = prompt('Введите число = ');
-    if(!input) 
-        break;
-    else
-        numbers.push(Number(input));
+while (true) {
+  input = prompt("Введите число = ");
+  if (!input) break;
+  else numbers.push(Number(input));
 }
 
-if(numbers.length !== 0)
-{
-    for(let i = 0; i< numbers.length; i++)
-    {
-        total += numbers[i];
-    }
+if (numbers.length !== 0) {
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
 }
 
 alert(`Общая сумма чисел = ${total}`);
 
 // task 2
 
-const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
 let attemptsLeft = 3;
 let usersAttempt;
 let boolCheck = true;
 
-while(boolCheck) 
-{
-    usersAttempt = prompt("Input password: ");
+while (boolCheck) {
+  usersAttempt = prompt("Input password: ");
 
-    if(attemptsLeft !==0){
-        for(let i = 0; i< passwords.length; i++)
-        {
-            if(usersAttempt === passwords[i])
-            {
-                alert('Добро пожаловать!');
-                boolCheck = false;
-                break;
-            }
-        }
-    if(boolCheck === true){
-            attemptsLeft --;
-            if(attemptsLeft > 0){
-                alert(`У вас осталось ${attemptsLeft} попыток!`);
-            }
-            else{
-                alert('У вас закончились попытки, аккаунт заблокирован!');
-                break;
-            }
-        }
+  if (attemptsLeft !== 0) {
+    for (let i = 0; i < passwords.length; i++) {
+      if (usersAttempt === passwords[i]) {
+        alert("Добро пожаловать!");
+        boolCheck = false;
+        break;
+      }
     }
+    if (boolCheck === true) {
+      attemptsLeft--;
+      if (attemptsLeft > 0) {
+        alert(`У вас осталось ${attemptsLeft} попыток!`);
+      } else {
+        alert("У вас закончились попытки, аккаунт заблокирован!");
+        break;
+      }
+    }
+  }
 }
---
+++ goit_fe_js/module3/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function(login) {
+  // код
+};
+
+const isLoginUnique = function(allLogins, login) {
+  // код
+};
+
+const addLogin = function(allLogins, login) {
+  // код
+};

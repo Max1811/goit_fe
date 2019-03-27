6bfb9e40b22c26c2087988e8579fec7985b8c1e5
Max <max.yar.eli@gmail.com>
2019-03-27 13:01:57 +0200

    v2

--- goit_fe_js/module3/scripts/app.js
+++ goit_fe_js/module3/scripts/app.js
@@ -1,15 +1,45 @@
-'use strict';
+"use strict";
 
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+// task 1
 
-const isLoginValid = function(login) {
-  // код
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = login => {
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const isLoginUnique = function(allLogins, login) {
-  // код
+const isLoginUnique = (allLogins, login) => {
+  for (const element of allLogins) {
+    if (element === login) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const addLogin = function(allLogins, login) {
-  // код
+  let isValid = isLoginValid(login);
+  if (isValid === false) {
+    console.log("Ошибка! Логин должен быть от 4 до 16 символов");
+  } else {
+    let isUnique = isLoginUnique(logins, login);
+    if (isUnique === false) {
+      console.log("Такой логин уже используется!");
+    } else {
+      allLogins.push(login);
+      console.log("Логин успешно добавлен!");
+      console.log(allLogins);
+    }
+  }
 };
+
+while (true) {
+  let newLogin = prompt("Input new login: ");
+  if (newLogin === null) break;
+
+  addLogin(logins, newLogin);
+}
